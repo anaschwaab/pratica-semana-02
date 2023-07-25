2f2a7428c642667acb25a422efa4afec4bcf9782
ana.goettens <goettensana@gmail.com>
2023-07-25 17:32:57 -0300

    closes #27-filter

---
+++ src/customFilter.ts
@@ -0,0 +1,13 @@
+type FilterCallBack<T> = (value: T, index: number, array: T[]) => boolean;
+
+export function customFilter<T>(array: T[], callback: FilterCallBack<T>): T[] {
+    const filteredArray: T[] = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            filteredArray.push(array[i]);
+        }
+    }
+
+    return filteredArray;
+}
