6de70e18f87c152c3afc63f210142dfd6662efa1
ana.goettens <goettensana@gmail.com>
2023-07-25 18:12:02 -0300

    closes #25-upperCaseFirst

--- src/upperCaseFirst.ts
+++ src/upperCaseFirst.ts
@@ -1,1 +1,9 @@
-export const upperCaseFirst = (text: string): string => text;
+export const upperCaseFirst = (text: string): string => {
+    const firstChar = text.charAt(0);
+    const restOfString = text.slice(1);
+
+    const result = firstChar.toUpperCase() + restOfString;
+
+    return result;
+
+};
