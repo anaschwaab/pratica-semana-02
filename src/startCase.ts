d1ebde69fb96c271338ed0a2315a24e241276adb
ana.goettens <goettensana@gmail.com>
2023-07-25 17:04:50 -0300

    closes #17-startCase

--- src/startCase.ts
+++ src/startCase.ts
@@ -5,5 +5,13 @@
  * @returns o texto enviado por parâmetro, alterado para startCase.
  */
 export const startCase = (word: string): string => {
-  return '';
+  const words = word.toLowerCase().split(/[\s_-]+/);
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+
+  const result = words.join(' ');
+
+  return result;
 };
